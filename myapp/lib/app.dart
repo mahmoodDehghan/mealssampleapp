5485192fafd619444cd80c019c4ed0db2b38a756
Mahmood Dehghan <38149357+mahmoodDehghan@users.noreply.github.com>
2021-01-20 18:21:36 +0330

    Initial commit

---
+++ myapp/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import './pages/homepage.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
